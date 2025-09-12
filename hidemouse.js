b19a3a5067f536f8a76f3b2f931dc0f52852517d
r128 <111167388+r128w@users.noreply.github.com>
2025-09-12 13:33:42 -0400

    make it work on page load immediately

--- hidemouse.js
+++ hidemouse.js
@@ -17,8 +17,10 @@ const hidemouse = {
 document.addEventListener('DOMContentLoaded', ()=>{
     let eles = document.querySelectorAll("."+hidemouse.tagname)
     for(var i = 0; i < eles.length; i ++){
+        // if(eles[i].parentElement.querySelector(":hover") === eles[i]){hidemouse.fn()}
         eles[i].addEventListener('mousemove', hidemouse.fn)
         eles[i].addEventListener('mouseup', hidemouse.fn)
         eles[i].addEventListener('mouseleave', hidemouse.fnLeave)
+        eles[i].addEventListener('mouseover', hidemouse.fn)
     }
 })
